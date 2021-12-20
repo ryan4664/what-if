a6991a5a94b1e10573850228cc3f3e3fd9ab59e8
Ryan Donohue <ryan@rdonohue.ca>
2021-12-19 21:27:39 -0600

    Tests

--- src/__tests__/services/ExperienceService.test.ts
+++ src/__tests__/services/ExperienceService.test.ts
@@ -3,7 +3,7 @@ import { ExperienceService } from '../../services/ExperienceService'
 import { MockContext, createMockContext } from '../context'
 import userFactory from '../factories/user'
 
-// import heroFactory from '../factories/hero'
+import heroFactory from '../factories/hero'
 
 let mockCtx: MockContext
 let experienceService: ExperienceService
@@ -15,7 +15,7 @@ beforeEach(() => {
 
 // User
 
-test('should gain experince but not level up ', async () => {
+test('should gain experience but not level up ', async () => {
   const user = userFactory.build()
   mockCtx.prisma.user.findUnique.mockResolvedValue(user)
 
@@ -30,10 +30,10 @@ test('should gain experince but not level up ', async () => {
 
   mockCtx.prisma.user.update.mockResolvedValue({
     ...user,
-    currentExperience: user.currentExperience + 500
+    currentExperience: user.currentExperience + 501
   })
 
-  const updatedUser = await experienceService.creditUserExperience({
+  await experienceService.creditUserExperience({
     userId: user.id,
     amountToCredit: 500
   })
@@ -49,11 +49,10 @@ test('should gain experince but not level up ', async () => {
     })
   )
 
-  expect(updatedUser?.currentExperience).toEqual(user.currentExperience + 500)
-  expect(updatedUser?.currentLevel).toEqual(user.currentLevel)
+  expect(mockCtx.prisma.user.update).toBeCalledTimes(1)
 })
 
-test('should gain experince and level up once ', async () => {
+test('should gain experience and level up once ', async () => {
   const user = userFactory.build()
   mockCtx.prisma.user.findUnique.mockResolvedValue(user)
   mockCtx.prisma.levelTier.findMany.mockResolvedValue([
@@ -61,6 +60,12 @@ test('should gain experince and level up once ', async () => {
       id: '1',
       level: 2,
       minExperience: 1000,
+      type: LevelType.User
+    },
+    {
+      id: '2',
+      level: 3,
+      minExperience: 10000,
       type: LevelType.User
     }
   ])
@@ -71,7 +76,7 @@ test('should gain experince and level up once ', async () => {
     currentLevel: user.currentLevel + 1
   })
 
-  const updatedUser = await experienceService.creditUserExperience({
+  await experienceService.creditUserExperience({
     userId: user.id,
     amountToCredit: 1500
   })
@@ -98,11 +103,10 @@ test('should gain experince and level up once ', async () => {
     })
   )
 
-  expect(updatedUser?.currentExperience).toEqual(1500)
-  expect(updatedUser?.currentLevel).toEqual(2)
+  expect(mockCtx.prisma.user.update).toBeCalledTimes(2)
 })
 
-test('should gain experince and level up twice ', async () => {
+test('should gain experience and level up twice ', async () => {
   const user = userFactory.build()
   mockCtx.prisma.user.findUnique.mockResolvedValue(user)
   mockCtx.prisma.levelTier.findMany.mockResolvedValue([
@@ -116,6 +120,12 @@ test('should gain experince and level up twice ', async () => {
       id: '2',
       level: 3,
       minExperience: 2000,
+      type: LevelType.User
+    },
+    {
+      id: '2',
+      level: 4,
+      minExperience: 20000,
       type: LevelType.User
     }
   ])
@@ -151,9 +161,11 @@ test('should gain experince and level up twice ', async () => {
       }
     })
   )
+
+  expect(mockCtx.prisma.user.update).toBeCalledTimes(2)
 })
 
-test('should gain experince and level up three times ', async () => {
+test('should gain experience and level up three times ', async () => {
   const user = userFactory.build()
   mockCtx.prisma.user.findUnique.mockResolvedValue(user)
   mockCtx.prisma.levelTier.findMany.mockResolvedValue([
@@ -173,6 +185,12 @@ test('should gain experince and level up three times ', async () => {
       id: '3',
       level: 4,
       minExperience: 5000,
+      type: LevelType.User
+    },
+    {
+      id: '4',
+      level: 5,
+      minExperience: 50000,
       type: LevelType.User
     }
   ])
@@ -208,180 +226,332 @@ test('should gain experince and level up three times ', async () => {
       }
     })
   )
+
+  expect(mockCtx.prisma.user.update).toBeCalledTimes(2)
+})
+
+test('should gain experience and hit max level ', async () => {
+  const user = userFactory.build()
+  mockCtx.prisma.user.findUnique.mockResolvedValue(user)
+  mockCtx.prisma.levelTier.findMany.mockResolvedValue([
+    {
+      id: '1',
+      level: 2,
+      minExperience: 1000,
+      type: LevelType.User
+    },
+    {
+      id: '2',
+      level: 3,
+      minExperience: 2000,
+      type: LevelType.User
+    },
+    {
+      id: '3',
+      level: 4,
+      minExperience: 5000,
+      type: LevelType.User
+    },
+    {
+      id: '4',
+      level: 5,
+      minExperience: 50000,
+      type: LevelType.User
+    }
+  ])
+
+  mockCtx.prisma.user.update.mockResolvedValue({
+    ...user,
+    currentExperience: user.currentExperience + 5000
+  })
+
+  await experienceService.creditUserExperience({
+    userId: user.id,
+    amountToCredit: 100000
+  })
+
+  expect(mockCtx.prisma.user.update).toBeCalledWith(
+    expect.objectContaining({
+      data: {
+        currentLevel: 5
+      },
+      where: {
+        id: user.id
+      }
+    })
+  )
+
+  expect(mockCtx.prisma.user.update).toBeCalledWith(
+    expect.objectContaining({
+      data: {
+        currentExperience: 50000
+      },
+      where: {
+        id: user.id
+      }
+    })
+  )
+
+  expect(mockCtx.prisma.user.update).toBeCalledTimes(2)
 })
 
 // Hero
 
-// test('should gain experince but not level up ', async () => {
-//   const hero = heroFactory.build()
-//   mockCtx.prisma.hero.findUnique.mockResolvedValue(hero)
+test('should gain experience but not level up ', async () => {
+  const hero = heroFactory.build()
+  mockCtx.prisma.hero.findUnique.mockResolvedValue(hero)
 
-//   mockCtx.prisma.levelTier.findMany.mockResolvedValue([
-//     {
-//       id: '1',
-//       level: 2,
-//       minExperience: 1000,
-//       type: LevelType.Hero
-//     }
-//   ])
+  mockCtx.prisma.levelTier.findMany.mockResolvedValue([
+    {
+      id: '1',
+      level: 2,
+      minExperience: 1000,
+      type: LevelType.Hero
+    }
+  ])
 
-//   const updatedHero = await experienceService.creditHeroExperience({
-//     heroId: hero.id,
-//     amountToCredit: 500
-//   })
+  await experienceService.creditHeroExperience({
+    heroId: hero.id,
+    amountToCredit: 500
+  })
 
-//   expect(mockCtx.prisma.hero.update).toBeCalledWith(
-//     expect.objectContaining({
-//       data: {
-//         currentExperience: 500
-//       },
-//       where: {
-//         id: hero.id
-//       }
-//     })
-//   )
+  expect(mockCtx.prisma.hero.update).toBeCalledWith(
+    expect.objectContaining({
+      data: {
+        currentExperience: 500
+      },
+      where: {
+        id: hero.id
+      }
+    })
+  )
 
-//   expect(updatedHero?.currentExperience).toEqual(hero.currentExperience + 500)
-//   expect(updatedHero?.currentLevel).toEqual(hero.currentLevel)
-// })
+  expect(mockCtx.prisma.hero.update).toBeCalledTimes(1)
+})
 
-// test('should gain experince and level up once ', async () => {
-//   const hero = heroFactory.build()
-//   mockCtx.prisma.hero.findUnique.mockResolvedValue(hero)
-//   mockCtx.prisma.levelTier.findMany.mockResolvedValue([
-//     {
-//       id: '1',
-//       level: 2,
-//       minExperience: 1000,
-//       type: LevelType.Hero
-//     }
-//   ])
+test('should gain experience and level up once ', async () => {
+  const hero = heroFactory.build()
+  mockCtx.prisma.hero.findUnique.mockResolvedValue(hero)
+  mockCtx.prisma.levelTier.findMany.mockResolvedValue([
+    {
+      id: '1',
+      level: 2,
+      minExperience: 1000,
+      type: LevelType.Hero
+    },
+    {
+      id: '2',
+      level: 3,
+      minExperience: 10000,
+      type: LevelType.Hero
+    }
+  ])
 
-//   const updatedHero = await experienceService.creditHeroExperience({
-//     heroId: hero.id,
-//     amountToCredit: 1500
-//   })
+  await experienceService.creditHeroExperience({
+    heroId: hero.id,
+    amountToCredit: 1500
+  })
 
-//   expect(mockCtx.prisma.hero.update).toBeCalledWith(
-//     expect.objectContaining({
-//       data: {
-//         currentLevel: 2
-//       },
-//       where: {
-//         id: hero.id
-//       }
-//     })
-//   )
+  expect(mockCtx.prisma.hero.update).toBeCalledWith(
+    expect.objectContaining({
+      data: {
+        currentLevel: 2
+      },
+      where: {
+        id: hero.id
+      }
+    })
+  )
 
-//   expect(mockCtx.prisma.hero.update).toBeCalledWith(
-//     expect.objectContaining({
-//       data: {
-//         currentExperience: 1500
-//       },
-//       where: {
-//         id: hero.id
-//       }
-//     })
-//   )
+  expect(mockCtx.prisma.hero.update).toBeCalledWith(
+    expect.objectContaining({
+      data: {
+        currentExperience: 1500
+      },
+      where: {
+        id: hero.id
+      }
+    })
+  )
 
-//   expect(updatedHero?.currentExperience).toEqual(1500)
-//   expect(updatedHero?.currentLevel).toEqual(2)
-// })
+  expect(mockCtx.prisma.hero.update).toBeCalledTimes(2)
+})
 
-// test('should gain experince and level up twice ', async () => {
-//   const hero = heroFactory.build()
-//   mockCtx.prisma.hero.findUnique.mockResolvedValue(hero)
-//   mockCtx.prisma.levelTier.findMany.mockResolvedValue([
-//     {
-//       id: '1',
-//       level: 2,
-//       minExperience: 1000,
-//       type: LevelType.Hero
-//     },
-//     {
-//       id: '2',
-//       level: 3,
-//       minExperience: 2000,
-//       type: LevelType.Hero
-//     }
-//   ])
+test('should gain experience and level up twice ', async () => {
+  const hero = heroFactory.build()
+  mockCtx.prisma.hero.findUnique.mockResolvedValue(hero)
+  mockCtx.prisma.levelTier.findMany.mockResolvedValue([
+    {
+      id: '1',
+      level: 2,
+      minExperience: 1000,
+      type: LevelType.Hero
+    },
+    {
+      id: '2',
+      level: 3,
+      minExperience: 2000,
+      type: LevelType.Hero
+    },
+    {
+      id: '2',
+      level: 4,
+      minExperience: 20000,
+      type: LevelType.Hero
+    }
+  ])
 
-//   await experienceService.creditHeroExperience({
-//     heroId: hero.id,
-//     amountToCredit: 2500
-//   })
+  await experienceService.creditHeroExperience({
+    heroId: hero.id,
+    amountToCredit: 2500
+  })
 
-//   expect(mockCtx.prisma.hero.update).toBeCalledWith(
-//     expect.objectContaining({
-//       data: {
-//         currentLevel: 3
-//       },
-//       where: {
-//         id: hero.id
-//       }
-//     })
-//   )
+  expect(mockCtx.prisma.hero.update).toBeCalledWith(
+    expect.objectContaining({
+      data: {
+        currentLevel: 3
+      },
+      where: {
+        id: hero.id
+      }
+    })
+  )
 
-//   expect(mockCtx.prisma.hero.update).toBeCalledWith(
-//     expect.objectContaining({
-//       data: {
-//         currentExperience: 2500
-//       },
-//       where: {
-//         id: hero.id
-//       }
-//     })
-//   )
-// })
+  expect(mockCtx.prisma.hero.update).toBeCalledWith(
+    expect.objectContaining({
+      data: {
+        currentExperience: 2500
+      },
+      where: {
+        id: hero.id
+      }
+    })
+  )
 
-// test('should gain experince and level up three times ', async () => {
-//   const hero = heroFactory.build()
-//   mockCtx.prisma.hero.findUnique.mockResolvedValue(hero)
-//   mockCtx.prisma.levelTier.findMany.mockResolvedValue([
-//     {
-//       id: '1',
-//       level: 2,
-//       minExperience: 1000,
-//       type: LevelType.Hero
-//     },
-//     {
-//       id: '2',
-//       level: 3,
-//       minExperience: 2000,
-//       type: LevelType.Hero
-//     },
-//     {
-//       id: '3',
-//       level: 4,
-//       minExperience: 5000,
-//       type: LevelType.Hero
-//     }
-//   ])
+  expect(mockCtx.prisma.hero.update).toBeCalledTimes(2)
+})
 
-//   await experienceService.creditHeroExperience({
-//     heroId: hero.id,
-//     amountToCredit: 5000
-//   })
+test('should gain experience and level up three times ', async () => {
+  const hero = heroFactory.build()
+  mockCtx.prisma.hero.findUnique.mockResolvedValue(hero)
+  mockCtx.prisma.levelTier.findMany.mockResolvedValue([
+    {
+      id: '1',
+      level: 2,
+      minExperience: 1000,
+      type: LevelType.Hero
+    },
+    {
+      id: '2',
+      level: 3,
+      minExperience: 2000,
+      type: LevelType.Hero
+    },
+    {
+      id: '3',
+      level: 4,
+      minExperience: 5000,
+      type: LevelType.Hero
+    },
+    {
+      id: '4',
+      level: 5,
+      minExperience: 50000,
+      type: LevelType.Hero
+    }
+  ])
 
-//   expect(mockCtx.prisma.hero.update).toBeCalledWith(
-//     expect.objectContaining({
-//       data: {
-//         currentLevel: 4
-//       },
-//       where: {
-//         id: hero.id
-//       }
-//     })
-//   )
+  await experienceService.creditHeroExperience({
+    heroId: hero.id,
+    amountToCredit: 5000
+  })
 
-//   expect(mockCtx.prisma.hero.update).toBeCalledWith(
-//     expect.objectContaining({
-//       data: {
-//         currentExperience: 5000
-//       },
-//       where: {
-//         id: hero.id
-//       }
-//     })
-//   )
-// })
+  expect(mockCtx.prisma.hero.update).toBeCalledWith(
+    expect.objectContaining({
+      data: {
+        currentLevel: 4
+      },
+      where: {
+        id: hero.id
+      }
+    })
+  )
+
+  expect(mockCtx.prisma.hero.update).toBeCalledWith(
+    expect.objectContaining({
+      data: {
+        currentExperience: 5000
+      },
+      where: {
+        id: hero.id
+      }
+    })
+  )
+
+  expect(mockCtx.prisma.hero.update).toBeCalledTimes(2)
+})
+
+test('should gain experience and hit max level ', async () => {
+  const hero = heroFactory.build()
+  mockCtx.prisma.hero.findUnique.mockResolvedValue(hero)
+  mockCtx.prisma.levelTier.findMany.mockResolvedValue([
+    {
+      id: '1',
+      level: 2,
+      minExperience: 1000,
+      type: LevelType.Hero
+    },
+    {
+      id: '2',
+      level: 3,
+      minExperience: 2000,
+      type: LevelType.Hero
+    },
+    {
+      id: '3',
+      level: 4,
+      minExperience: 5000,
+      type: LevelType.Hero
+    },
+    {
+      id: '4',
+      level: 5,
+      minExperience: 50000,
+      type: LevelType.Hero
+    }
+  ])
+
+  mockCtx.prisma.hero.update.mockResolvedValue({
+    ...hero,
+    currentExperience: hero.currentExperience + 5000
+  })
+
+  await experienceService.creditHeroExperience({
+    heroId: hero.id,
+    amountToCredit: 100000
+  })
+
+  expect(mockCtx.prisma.hero.update).toBeCalledWith(
+    expect.objectContaining({
+      data: {
+        currentLevel: 5
+      },
+      where: {
+        id: hero.id
+      }
+    })
+  )
+
+  expect(mockCtx.prisma.hero.update).toBeCalledWith(
+    expect.objectContaining({
+      data: {
+        currentExperience: 50000
+      },
+      where: {
+        id: hero.id
+      }
+    })
+  )
+
+  expect(mockCtx.prisma.hero.update).toBeCalledTimes(2)
+})
