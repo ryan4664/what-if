986c95c9793ee8c9ec0541d770e9c0a39abde517
Ryan Donohue <ryan@rdonohue.ca>
2022-01-18 22:27:39 -0600

    Init frontend

---
+++ frontend/why-not/src/App.tsx
@@ -0,0 +1,43 @@
+import { gql, useQuery } from '@apollo/client'
+import './App.css'
+
+const GET_USERS = gql`
+  query GetUsers {
+    users {
+      id
+      emailAddress
+      currentLevel
+      currentExperience
+      timeShards
+      heros {
+        id
+        multiverse
+        name
+        attributeName
+        totalHealth
+        currentHealth
+        speach
+        currentExperience
+        currentLevel
+      }
+    }
+  }
+`
+
+function App() {
+  const { loading, error, data } = useQuery(GET_USERS)
+
+  console.log(loading)
+  console.log(error)
+  console.log(data)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Why the fuck not!</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
