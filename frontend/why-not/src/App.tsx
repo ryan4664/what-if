952a05c3b41f450bcf5552624f21360ef3561108
Ryan Donohue <ryan@rdonohue.ca>
2022-03-17 20:57:19 -0600

    Added react router

--- frontend/why-not/src/App.tsx
+++ frontend/why-not/src/App.tsx
@@ -1,6 +1,12 @@
 import { gql, useQuery } from '@apollo/client'
 import './App.css'
 import { ChakraProvider } from '@chakra-ui/react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link
+} from "react-router-dom";
 
 const GET_USERS = gql`
   query GetUsers {
@@ -29,11 +35,24 @@ function App() {
   const { loading, error, data } = useQuery(GET_USERS)
 
   return (
+    <Router>
     <ChakraProvider>
       <header className="App-header">
         <p>Why the fuck not!</p>
       </header>
+      <Routes >
+          <Route path="/about">
+            {/* <About /> */}
+          </Route>
+          <Route path="/users">
+            {/* <Users /> */}
+          </Route>
+          <Route path="/">
+            {/* <Home /> */}
+          </Route>
+        </Routes >
     </ChakraProvider>
+    </Router>
   )
 }
 
