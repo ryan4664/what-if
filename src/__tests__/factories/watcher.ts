4ce48ba62eb73a2e21302ecbd8f116073374401d
Ryan Donohue <ryan@rdonohue.ca>
2023-01-14 14:03:38 -0600

    Watcher

---
+++ src/__tests__/factories/watcher.ts
@@ -0,0 +1,11 @@
+import {Factory} from 'fishery'
+import {v4 as uuidv4} from 'uuid'
+import {Random} from 'random-js'
+import {Watcher} from "@prisma/client";
+
+const random = new Random()
+
+export default Factory.define<Watcher>(() => ({
+  id: uuidv4(),
+  timeShards: random.integer(0, 10000),
+}))
---
+++ src/__tests__/services/WatcherService.test.ts
@@ -0,0 +1,24 @@
+import {createMockContext, MockContext} from '../context'
+import {WatcherService} from "../../services/WatcherService"
+import watcherFactor from  '../factories/watcher'
+
+let mockCtx: MockContext
+let watcherService: WatcherService
+
+beforeEach(() => {
+  mockCtx = createMockContext()
+  mockCtx.prisma.watcher.findFirst.mockResolvedValue(null)
+  watcherService = new WatcherService(mockCtx.prisma)
+})
+
+test('should throw an error ', async () => {
+  await expect(watcherService.getWatcher()).rejects.toThrowError()
+})
+
+test('should return the watcher ', async () => {
+  const watcher = watcherFactor.build()
+  mockCtx.prisma.watcher.findFirst.mockResolvedValue(watcher)
+
+  const result = await watcherService.getWatcher()
+  expect(result).not.toBeNull()
+})
