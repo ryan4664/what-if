9208fb21c0d8f960f3bf7c6782168f8f51261ca8
Ryan Donohue <ryan@rdonohue.ca>
2023-01-28 14:15:26 -0600

    Handling watcher goals

--- src/services/WatcherService.ts
+++ src/services/WatcherService.ts
@@ -17,4 +17,50 @@ export class WatcherService {
         return watcher
     }
 
+    public creditTimeShards = async (amount: number): Promise<void> => {
+        let watcher = await this.prisma.watcher.findFirst({
+            include: {
+                timeShardGoals: true
+            }
+        })
+
+        if (!watcher) {
+            throw new Error("No Watcher Configured!")
+        }
+
+        const updatedTimeShards = watcher.timeShards + amount
+
+        await this.prisma.watcher.update({
+            data: {
+                timeShards: updatedTimeShards
+            },
+            where: {
+                id: watcher.id
+            }
+        })
+
+        const triggeredGoals = watcher.timeShardGoals.filter(goal =>
+            !goal.isUnlocked &&
+            updatedTimeShards >= goal.timeShardsRequired)
+
+        const result = triggeredGoals.map(goal => this.unlockGoal(goal))
+
+        await Promise.all(result)
+
+        return
+    }
+
+    public unlockGoal = async (goal) => {
+        //     do whatever the goal should do
+
+        //     mark the goal as triggered
+        this.prisma.watcherTimeShardGoals.update({
+            data: {
+                isUnlocked: true
+            },
+            where: {
+                id: goal.id
+            }
+        })
+    }
 }
